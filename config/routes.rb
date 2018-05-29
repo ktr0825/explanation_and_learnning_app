0f72e540b27f148214eabc90103d1a647bd277a9
ktr5280 <jascine@icloud.com>
2018-05-29 10:19:29 +0900

    Add MainPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'main_pages/home'
+
+  get 'main_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
