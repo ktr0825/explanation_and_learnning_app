79e7e620f33012acc014da5900d84cb6eab492f2
ktr5280 <jascine@icloud.com>
2018-05-29 13:51:26 +0900

    make signup page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main_pages#home'
   get '/help', to: 'main_pages#help'
+  get '/signup', to: 'users#new'
+
+  resources :users
 end
